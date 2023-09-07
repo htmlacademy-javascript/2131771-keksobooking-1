97d8315aa0f63b7cfde94e5b38513bbbaed79976
StanislavK <gunmanmain@gmail.com>
2023-09-07 15:48:22 +0300

    Добавляет недостающие решения

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,61 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
+// Функция для проверки, является ли строка палиндромом.
+
+function isPalindrome(str) {
+  const reverseString = str.split('').reverse().join('');
+
+  if (str === reverseString) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-console.log(reverseString("academy"));
+
+// Функция, которая возвращает числа из строки в виде целого положительного числа.
+function extractNumbersSum(str) {
+  str = str.split('');
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (parseInt(str[i], 10)) {
+      sum = sum + parseInt(str[i], 10);
+    }
+  }
+  return sum;
+}
 
 
+// Функция, возвращающая исходную строку, дополненную указанными символами до заданной длины.
 
-function randomNumber(fromNumber, toNumber, decimalPlaces) {
+// принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами
+// возвращает исходную строку, дополненную указанными символами до заданной длины.
+//Символы добавляются в начало строки.
+//Если исходная строка превышает заданную длину, она не должна обрезаться.
+//Если «добивка» слишком длинная, она обрезается с конца.
+
+functuion returnFullString(srt, srtMinLenght, strFillSymbols) {
+  if (str.length() >= srtMinLenght.length()) {
+    return str;
+  }
+  else {
+  let emptySymbols = srtMinLenght.length() - str.length();
+
+  for (let i = 0; i < emptySymbols; i++) {
+    strFillSymbols.slice(0,i);
+  }
+  return
+  }
+}
+
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+
+function getRandomNumber(fromNumber, toNumber, decimalPlaces) {
   if ((fromNumber || toNumber) < 0) {
     return NaN;
   }
-else {
-  return Math.random() * (toNumber - fromNumber) + fromNumber;
+  else {
+    let result = Math.random() * (toNumber - fromNumber) + fromNumber;
+    return result.toFixed(decimalPlaces);
+  }
 }
-}
-
-console.log(randomNumber(55, 3, 3));
