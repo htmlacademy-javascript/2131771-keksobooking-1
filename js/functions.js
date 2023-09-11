0089d3d98c618458de85976119bac9ee9a82777b
StanislavK <gunmanmain@gmail.com>
2023-09-11 11:57:24 +0300

    Исправляет ошибки в работе функций и в оформлении

--- js/functions.js
+++ js/functions.js
@@ -3,66 +3,48 @@
 function isPalindrome(str) {
   const reverseString = str.split('').reverse().join('');
 
-  if (str === reverseString) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  return str === reverseString;
 }
 
 
 // Функция, которая возвращает числа из строки в виде целого положительного числа.
 function extractNumbersSum(str) {
-  str = str.split('');
   let sum = 0;
+
   for (let i = 0; i < str.length; i++) {
     if (parseInt(str[i], 10)) {
-      sum = sum + parseInt(str[i], 10);
+      sum += parseInt(str[i], 10);
     }
   }
+
   return sum;
 }
 
 
 // Функция, возвращающая исходную строку, дополненную указанными символами до заданной длины.
 
-function returnFullString(str, srtMinLenght, strFillSymbols) {
-  if (str.length >= srtMinLenght) {
-    return str;
-  }
-  else {
-    let emptySymbols = srtMinLenght - str.length;
-    if (strFillSymbols.length <= srtMinLenght) {
-      for (let i = 1; i <= emptySymbols; i++) {
-        const symbolsToFill = strFillSymbols.slice(0,i);
-        const result = symbolsToFill + str;
-      }
-      return result;
-    }
-    else {
-      let emptySymbols = srtMinLenght - str.length;
-      if (strFillSymbols.length >= srtMinLenght) {
-        for (let i = 1; i <= emptySymbols; i++) {
-          let symbolsToFill = strFillSymbols.slice(0,i);
-          let result = symbolsToFill + str;
-        }
+function addPaddingToString(str, minLength, padding) {
+  if (str.length >= minLength) {
 
-        return result;
-      }
-    }
+    return str;
+  } else {
+    const paddingLength = minLength - str.length;
+    const trimmedPadding = padding.slice(-paddingLength);
+    const result = trimmedPadding + str;
+
+    return result;
   }
 }
-
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
 function getRandomNumber(fromNumber, toNumber, decimalPlaces) {
   if ((fromNumber || toNumber) < 0) {
+
     return NaN;
   }
-  else {
-    let result = Math.random() * (toNumber - fromNumber) + fromNumber;
-    return result.toFixed(decimalPlaces);
-  }
+
+  const result = Math.random() * (toNumber - fromNumber) + fromNumber;
+
+  return result.toFixed(decimalPlaces);
 }
